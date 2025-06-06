b76c645e4cd775c630405f63de9400f65f95cace
Rifqi Harits <rharits18@gmail.com>
2025-06-06 18:08:38 +0700

    add LoginScreen

---
+++ app/src/main/java/com/tugasbesar/pemesananmakanan/ui/auth/LoginScreen.kt
@@ -0,0 +1,52 @@
+package com.tugasbesar.pemesananmakanan.ui.auth
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(
+    onLoginSuccess: () -> Unit,
+    onNavigateToRegister: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+
+        Text("Login", style = MaterialTheme.typography.titleLarge)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { onLoginSuccess() }, // ganti dengan validasi nanti
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        TextButton(onClick = { onNavigateToRegister() }) {
+            Text("Belum punya akun? Daftar")
+        }
+    }
+}
